b4b15df7e1803ae3e398f220b4b66de48deb9df9
Michal Papis <mpapis@gmail.com>
2012-01-30 07:05:40 +0100

    test comment_test_input plugin

---
+++ test/unit/comment_test_input.rb
@@ -0,0 +1,87 @@
+require 'minitest/autorun'
+require 'plugins/dtf/comment_test_input'
+
+class TestCommentTestInput < MiniTest::Unit::TestCase
+  def setup
+    @test = DTF::CommentTestInput.new
+    @file1 = Tempfile.new(['','_comment_test.sh'])
+    @file2 = Tempfile.new(['','_wrong_test.sh'])
+  end
+
+  def teardown
+    @file1.unlink
+    @file2.unlink
+  end
+  
+  def file_open file, &block
+    file.open
+    block.call
+  ensure
+    file.close
+  end
+
+  def test_matches
+    assert @test.matches?(@file1.path), "'#{@file1.path}' did not matched"
+    assert !@test.matches?(@file2.path), "'#{@file2.path}' matched but should not"
+    assert !@test.matches?("/path/to/file.rb")
+    assert !@test.matches?("/path/to/file_comment_test.rb")
+    assert !@test.matches?("anything")
+  end
+
+  def test_reading_two_different_lines
+    file_open @file1 do
+      @file1.write "true # status=0\n"
+      @file1.write "false # status!=0; match=/^$/\n"
+    end
+    data = @test.load @file1.path
+    assert !data[:commands].empty?, "Commands missing '#{data}'."
+    assert_equal data[:commands].size, 2, "Wrong number of commands '#{data[:commands]}'."
+    assert !data[:commands][0][:cmd].empty?, "Command missing '#{data[:commands][0]}'."
+    assert !data[:commands][0][:tests].empty?, "Tests missing '#{data[:commands][0]}'."
+    assert_equal data[:commands].map{|line| line[:tests].size }.inject(&:+), 3, "Wrong number of tests '#{data[:commands]}'."
+  end
+
+  def test_reading_two_same_commands
+    file_open @file1 do
+      @file1.write "true # status=0\n"
+      @file1.write "true # status!=0; match=/^$/\n"
+    end
+    data = @test.load @file1.path
+    assert_equal data[:commands].size, 2, "Wrong number of commands '#{data[:commands]}'."
+    assert_equal data[:commands].map{|line| line[:tests].size }.inject(&:+), 3, "Wrong number of tests '#{data[:commands]}'."
+  end
+
+  def test_reading_empty_lines
+    file_open @file1 do
+      @file1.write "true # status=0\n"
+      @file1.write "\n"
+      @file1.write "true # status!=0; match=/^$/\n"
+    end
+    data = @test.load @file1.path
+    assert_equal data[:commands].size, 2, "Wrong number of commands '#{data[:commands]}'."
+    assert_equal data[:commands].map{|line| line[:tests].size }.inject(&:+), 3, "Wrong number of tests '#{data[:commands]}'."
+  end
+
+  def test_reading_user_comments
+    file_open @file1 do
+      @file1.write "##true # status=0\n"
+      @file1.write "true # status!=0; match=/^$/ ## nothing to add\n"
+    end
+    data = @test.load @file1.path
+    assert_equal data[:commands].size, 1, "Wrong number of commands '#{data[:commands]}'."
+    assert_equal data[:commands].map{|line| line[:tests].size }.inject(&:+), 2, "Wrong number of tests '#{data[:commands]}'."
+  end
+
+  def test_reading_multiline_tests
+    file_open @file1 do
+      @file1.write "true"
+      @file1.write "# status=0\n"
+      @file1.write "true\n"
+      @file1.write "# status!=0\n"
+      @file1.write "# match=/^$/\n"
+    end
+    data = @test.load @file1.path
+    assert_equal data[:commands].size, 2, "Wrong number of commands '#{data[:commands]}'."
+    assert_equal data[:commands].map{|line| line[:tests].size }.inject(&:+), 3, "Wrong number of tests '#{data[:commands]}'."
+  end
+end
