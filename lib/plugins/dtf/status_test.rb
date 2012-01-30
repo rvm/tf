44826aea299e1d6bdb30a8356f380b23748025c4
Michal Papis <mpapis@gmail.com>
2012-01-30 01:59:59 +0100

    test status_test plugin

--- lib/plugins/dtf/status_test.rb
+++ lib/plugins/dtf/status_test.rb
@@ -1,13 +1,12 @@
 class DTF::StatusTest
-  def initialize
-  end
+  MATCHER = /^status([!]?=)([[:digit:]]+)$/
 
   def matches? test
-    test =~ /^status([!]?=)(.*)/
+    test =~ DTF::StatusTest::MATCHER
   end
 
   def execute test, _stdout, _stderr, _stdboth, _status, env
-    test =~ /^status([!]?=)(.*)/
+    test =~ DTF::StatusTest::MATCHER
     sign, value = $1, $2.to_i
     if ( sign == "=" ) ^ ( _status == value )
       [ false, "failed: status #{sign} #{value} # was #{_status}" ]
---
+++ test/unit/status_test.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require 'plugins/dtf/status_test'
+
+class TestStatusTest < MiniTest::Unit::TestCase
+  def setup
+    @test = DTF::StatusTest.new
+  end
+  def test_matches
+    assert @test.matches?("status=0")
+    assert @test.matches?("status!=0")
+    assert @test.matches?("status=1")
+    assert @test.matches?("status!=1")
+    assert ! @test.matches?("status=a")
+    assert ! @test.matches?("status=")
+    assert ! @test.matches?("astatus!=1")
+  end
+
+  def test_status_success
+    result, msg = @test.execute "status=0", "", "", "", 0, {}
+    assert result
+    assert_equal msg, "passed: status = 0"
+  end
+  def test_status_failed
+    result, msg = @test.execute "status=0", "", "", "", 1, {}
+    assert ! result
+    assert_equal msg, "failed: status = 0 # was 1"
+  end
+  def test_status_not_success
+    result, msg = @test.execute "status!=0", "", "", "", 1, {}
+    assert result
+    assert_equal msg, "passed: status != 0"
+  end
+  def test_status_not_failed
+    result, msg = @test.execute "status!=0", "", "", "", 0, {}
+    assert ! result
+    assert_equal msg, "failed: status != 0 # was 0"
+  end
+end
