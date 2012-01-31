abd35eef02601acb8900fbd8cca006f3f78f4f79
Michal Papis <mpapis@gmail.com>
2012-01-31 05:31:22 +0100

    status_output plugin - print status line on end

---
+++ lib/plugins/dtf/stats_output.rb
@@ -0,0 +1,69 @@
+class DTF::StatsOutput
+  RED = `tput setaf 1`
+  GREEN = `tput setaf 2`
+  YELLOW = `tput setaf 3`
+  BLUE = `tput setaf 4`
+  RESET = `tput setaf 9`
+
+  def initialize
+    @counts={}
+    @counts[:commands] = 0
+    @counts[:tests] = 0
+    @counts[:commands_finished] = 0
+    @counts[:tests_success] = 0
+    @counts[:tests_failure] = 0
+  end
+
+  def start_processing
+  end
+
+  def status
+    text = "#{BLUE}##### Processed commands #{@counts[:commands_finished]} of #{@counts[:commands]}"
+    if @counts[:tests_success] > 0
+      text += ", #{GREEN}success tests #{@counts[:tests_success]} of #{@counts[:tests]}"
+    end
+    if @counts[:tests_failure] > 0
+      text += ", #{RED}failure tests #{@counts[:tests_failure]} of #{@counts[:tests]}"
+    end
+    skipped = @counts[:tests] - @counts[:tests_success] - @counts[:tests_failure]
+    if skipped > 0
+      text += ", #{YELLOW}skipped tests #{skipped} of #{@counts[:tests]}"
+    end
+    text += ".#{RESET}"
+    text
+  end
+
+  def end_processing
+    puts status
+  end
+
+  def start_test test, env
+    @counts[:commands] += test[:commands].size
+    tests_counts = test[:commands].map{|line| line[:tests].nil? ? 0 : line[:tests].size }
+    @counts[:tests] += tests_counts.empty? ? 0 : tests_counts.inject(&:+)
+  end
+
+  def end_test test
+  end
+
+  def start_command line
+  end
+
+  def end_command line, status, env
+    @counts[:commands_finished] += 1
+  end
+
+  def command_out out
+  end
+
+  def command_err err
+  end
+
+  def test_processed test, status, msg
+    if status
+      @counts[:tests_success] += 1
+    else
+      @counts[:tests_failure] += 1
+    end
+  end
+end
---
+++ test/unit/stats_output.rb
@@ -0,0 +1,69 @@
+require 'minitest/autorun'
+require 'plugins/dtf/stats_output'
+
+class TestStatsOutput < MiniTest::Unit::TestCase
+  def setup
+    @test = DTF::StatsOutput.new
+  end
+
+  # Build input data, will create command for every integer passed,
+  # command will contain tests number equal to passed integer
+  def build_input *arr
+    arr.flatten!
+    { :commands => arr.map{|count|
+      { :tests => count==0 ? [] : (1..count).to_a }
+    } }
+  end
+
+  def test_empty
+    assert @test.status =~ /##### Processed commands 0 of 0/
+  end
+
+  def test_not_processed
+    @test.start_test(build_input(0), {})
+
+    assert_match /##### Processed commands 0 of 1/, @test.status
+    refute_match /(success|failure|skipped)/, @test.status
+  end
+
+  def test_processed_success
+    @test.start_test(build_input(1), {})
+    @test.end_command( '', true, {} )
+    @test.test_processed( {}, true, '' )
+
+    assert_match /##### Processed commands 1 of 1/, @test.status
+    assert_match /success tests 1 of 1/, @test.status
+    refute_match /(failure|skipped)/, @test.status
+  end
+
+  def test_processed_failure
+    @test.start_test(build_input(1), {})
+    @test.end_command( '', true, {} )
+    @test.test_processed( {}, false, '' )
+
+    assert_match /##### Processed commands 1 of 1/, @test.status
+    assert_match /failure tests 1 of 1/, @test.status
+    refute_match /(successskipped)/, @test.status
+  end
+
+  def test_processed_skipped
+    @test.start_test(build_input(1), {})
+    @test.end_command( '', true, {} )
+
+    assert_match /##### Processed commands 1 of 1/, @test.status
+    assert_match /skipped tests 1 of 1/, @test.status
+    refute_match /(success|failure)/, @test.status
+  end
+
+  def test_processed_all
+    @test.start_test(build_input(1,2), {})
+    2.times{ @test.end_command('',true,{}) }
+    @test.test_processed( {}, true, '' )
+    @test.test_processed( {}, false, '' )
+
+    assert_match /##### Processed commands 2 of 2/, @test.status
+    assert_match /success tests 1 of 3/, @test.status
+    assert_match /failure tests 1 of 3/, @test.status
+    assert_match /skipped tests 1 of 3/, @test.status
+  end
+end
