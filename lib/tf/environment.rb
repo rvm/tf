345c64b14b37b50cbda8cdd5e8790979269da742
Michal Papis <mpapis@gmail.com>
2012-08-08 08:21:57 +0200

    TF is a class

--- lib/tf/environment.rb
+++ lib/tf/environment.rb
@@ -18,9 +18,8 @@
 #~  now'
 
 
-module TF
-  class Environment
-    HANDLER=<<EOF
+class TF::Environment
+  HANDLER=<<EOF
 __tf_show_env_tool()
 {
 typeset v x y
@@ -41,38 +40,37 @@ do
 done
 }
 EOF
-    class << self
-      def define_handler
-        TF::Environment::HANDLER
-      end
-      def handler_command
-        '__tf_show_env_tool'
-      end
-      def parse_env output
-        holder=nil
-        terminator=nil
-        output.each do |line|
-          if holder.nil? && line =~ /^[^=]=([\('\$]?)/
-            holder = line
-            until $1.nil?
-              terminator = $1.sub(/$/,"'").sub(/\(/,")")
-              terminator << '$'
-              terminator = Regexp.new(terminator)
-            end
-          else
-            holder += line
+  class << self
+    def define_handler
+      TF::Environment::HANDLER
+    end
+    def handler_command
+      '__tf_show_env_tool'
+    end
+    def parse_env output
+      holder=nil
+      terminator=nil
+      output.each do |line|
+        if holder.nil? && line =~ /^[^=]=([\('\$]?)/
+          holder = line
+          until $1.nil?
+            terminator = $1.sub(/$/,"'").sub(/\(/,")")
+            terminator << '$'
+            terminator = Regexp.new(terminator)
           end
-          if terminator && line =~ terminator
-            terminator=nil
-          end
-          if holder && terminator.nil?
-            parse_var holder
-            holder=nil
-          end
+        else
+          holder += line
+        end
+        if terminator && line =~ terminator
+          terminator=nil
+        end
+        if holder && terminator.nil?
+          parse_var holder
+          holder=nil
         end
       end
-      def parse_var definition
-      end
+    end
+    def parse_var definition
     end
   end
 end
